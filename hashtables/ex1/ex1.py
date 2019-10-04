351147f0282f46a8a98fb474f077470ca7e9ff15
pdxdave <webdevdave@hotmail.com>
2019-10-04 08:10:32 -0700

    first commit

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,4 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
+#  What if we store each weight in the input list as keys? What would be a useful thing to store as the value for each key? 
+#  If we store each weight's list index as its value, we can then check to see if the hash table contains an entry for `limit - weight`. If it does, then we've found the two items whose weights sum up to the `limit`!
+
 from hashtables import (HashTable,
                         hash_table_insert,
                         hash_table_remove,
